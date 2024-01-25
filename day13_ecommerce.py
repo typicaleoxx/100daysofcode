926ce74de746e35a7a937925f824db79a14683f3
typicaleoxx <lsneha991@gmail.com>
2024-01-25 22:29:52 +0545

    modified ecommerce

--- day13_ecommerce.py
+++ day13_ecommerce.py
@@ -17,15 +17,20 @@ def main():
 def register():
     username=input("Please enter your username: ")
     password=input("Please enter your password: ")
-    user_type=input("Are you a buyer or a seller(buyer/seller)").lower()
+    user_type=input("Are you a buyer or a seller(buyer/seller)").lower().strip()
     try:
         if user_type=="buyer" or user_type=="seller":
-            user_data={username:password, "user_type":user_type}
+            user_data={"username":username, "password":password, "user_type":user_type}
             file=open("user_data.json","a")
             try:
                 json.dump(user_data,file)
                 print("Registration successfull")
                 file.close()
+                login=input("Do you want to login(y/n)").lower().strip()
+                if login=="y":
+                    login()
+                else:
+                    print(f"Alright {username}, have a great day.")
             except:
                 print("Registration error")
     except:
@@ -37,7 +42,7 @@ def login():
     file=open("user_data.json","r")
     for data in file:
         user_data=json.loads(data)
-        if user_data[username]==password:
+        if user_data.get("username")==username and user_data.get("password")==password:
             print("Login Successful !")
             if user_data["user_type"]=="buyer":
                 buyer_menu()
@@ -46,6 +51,38 @@ def login():
             break
         else:
             print("Invalid username or password. ")
+def seller_menu():
+    try:
+        print("Seller Menu")
+        print("1. Add products")
+        print("2. View your products")
+        print("3. View your product bills")
+        print("4. Delete products")
+        user_choice=input("Enter your choice (1/2/3)")
+        if user_choice in [1,2,3,4]:
+            if user_choice==1:
+                add_products()
+            elif user_choice==2:
+                view_your_products()
+            elif user_choice==3:
+                product_bills()
+        else: 
+            print("Invalid choice")
+    except:
+        print("An error occured. ")
+
+
+def add_products():
+    pass
+
+def view_your_products():
+    pass
+
+def purchase_products():
+    pass
+
+def product_bills():
+    pass
 
 def buyer_menu():
     try:
@@ -57,21 +94,18 @@ def buyer_menu():
         if user_choice in [1,2,3]:
             if user_choice==1:
                 view_all_products()
-            else user_choice==2:
+            elif user_choice==2:
                 view_user_bill()
+            else:
+                purchase_products()
         else: 
-            print()
+            print("Invalid choice.")
     except:
         print("Error occured")
+def view_all_products():
+    pass
+def view_user_bill():
+    pass
 
-def seller_menu():
-    try:
-        print("Seller Menu")
-        print("1. Add products")
-        print("2. View your products")
-        print("3. View your product bills")
-        print("4. Delete products")
-    except:
-        print("An error occured. ")
 main()
             
---
+++ ecommerce.py
@@ -0,0 +1,139 @@
+# Requirements
+# Ask the user if he/she wants to login or register
+# Register - Ask username, password, usertype (Buyer/Seller) and store it in a file
+# Login - Ask username and password , if the username and password exists on userdata file then print login successfull. Check the usertype.
+# If the user is buyer , give him choices : View all products, View his/her bills, Purchase products. If the user is seller : Add products, View his/her products, View his/her products bills
+
+# User data = {"username":"ram","password":"ram123","usertype":"buyer"}
+
+# {'username':'password','usertype':'buyer'}
+#last requirement- seller le ni bill herne 
+import json
+
+def register():
+    user_name = input("Enter your username: ")
+    user_password = input("Create a password: ")
+    user_type = input("Are you a buyer or seller? ").lower()
+
+    user_data = {"user_name": user_name, "user_password": user_password, "user_type" :user_type }
+    json_user_data = json.dumps(user_data)
+
+    f = open("D:/Work/MindrisersTeaching/Poush/file/userdata.txt","a")
+    f.write(json_user_data + "-")
+    f.close()
+    user_log = input("Do you want to login? [y/n]").lower()
+    if user_log == "y":
+        login()
+    else:
+        print("thankyou for the registration!!")
+
+def login():
+    user_name = input("Enter your username: ")
+    user_password = input("Enter your password: ")
+    
+    f = open("D:/Work/MindrisersTeaching/Poush/file/userdata.txt","r")
+    json_user_datas = f.read()
+    f.close()
+    list_user_data = json_user_datas.split("-")
+    user_login = False
+    for i in list_user_data:
+        if i!= '':
+            dict_data = json.loads(i)
+            if (user_name == dict_data.get("user_name")) and user_password == dict_data.get("user_password"):
+                user_login = True
+                type = dict_data.get("user_type") 
+                break
+            
+    if user_login == True:
+        print("Login Successful!!")
+        if type == "buyer":
+            print(f"Hi {dict_data.get('user_name')}! Welcome to your buyer account.")
+            print("What would you like to do? ")
+            user_operation = input("[view products/ purchase/ view bills] :").lower() 
+            if user_operation == "view product" or user_operation == "view products":
+                view_all_products()   
+            elif user_operation == "view bill" or user_operation == "view bills":
+                view_bills()
+            elif user_operation == "purchase":
+                purchase()
+            else:
+                print("Invalid selection!!!")
+        else:
+            print(f"Hi {dict_data.get('user_name')}! Welcome to your seller account.")
+            print("What would you like to do? ")
+            user_operation = input("[add product/ view bills] :").lower() 
+            if user_operation == "add product" or user_operation == "add products":
+                add_product()
+            elif user_operation == "view bill" or user_operation == "view bills":
+                view_bills(user_name)
+            else:
+                print("Invalid selection!!!")
+        
+    else:
+        print("Invalid Credentials!!")                              
+
+def view_bills():
+    pass
+
+def purchase():
+    purchase_product_name = input('Which product do you want to buy? ')
+    purhcase_quantity = int(input('How much quantity? '))
+
+    f = open('D:/Work/MindrisersTeaching/Poush/file/productdata.txt','r')
+
+    product_json_data = f.read()
+
+    f.close()
+
+    list_product_data = product_json_data.split('-')
+
+    for i in list_product_data:
+        if i != '':
+            dict_data = json.loads(i)
+            if purchase_product_name == dict_data['product_name']:
+                price = int(dict_data['product_price'])
+
+    print('Purchase completed!')
+
+    print(f'Your total is {purhcase_quantity * price}')
+
+
+
+def view_all_products():
+    f = open('D:/Work/MindrisersTeaching/Poush/file/productdata.txt','r')
+
+    product_json_data = f.read()
+
+    f.close()
+
+    list_product_data = product_json_data.split('-')
+
+    for i in list_product_data:
+        print(i)
+
+    purchase()
+
+def view_products():
+    pass
+
+def add_product():
+    product_name = input('Enter product name : ')
+    product_description = input('Enter product description : ')
+    product_price = input('Enter product price : ')
+
+    product_dict_data = {'product_name':product_name,'product_desc':product_description,'product_price':product_price}
+    product_json_data = json.dumps(product_dict_data)
+    f = open('D:/Work/MindrisersTeaching/Poush/file/productdata.txt','a')
+
+    f.write(product_json_data + '-')
+
+    f.close()
+
+
+user_input = input("Do you want to login or register? ").lower()
+
+if user_input == "register":
+    register()
+
+if user_input == "login":
+    login()
